2f84d745d499d5c77e3952bf2662775375b9668e
jonathanvdc <jonathan.vdc@outlook.com>
2017-01-30 12:38:31 +0100

    Define the ICommand interface

---
+++ src/Flake/ICommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Flame.Compiler;
+
+namespace Flake
+{
+    /// <summary>
+    /// Defines commands: sub-programs that can be run independently,
+    /// and parse their own input.
+    /// </summary>
+    public interface ICommand
+    {
+        /// <summary>
+        /// Gets the command's name.
+        /// </summary>
+        /// <value>The command's name.</value>
+        string Name { get; }
+
+        /// <summary>
+        /// Runs this command with the given arguments and log.
+        /// </summary>
+        /// <param name="Arguments">The command's arguments.</param>
+        /// <param name="Log">The log to which diagnostics can be sent.</param>
+        void Run(string[] Arguments, ICompilerLog Log);
+    }
+}
+
