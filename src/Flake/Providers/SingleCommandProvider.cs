686c87beb1b841340d1500824dee041fc40c848a
jonathanvdc <jonathan.vdc@outlook.com>
2017-01-30 22:27:01 +0100

    Define Single*Provider classes

---
+++ src/Flake/Providers/SingleCommandProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Flame.Compiler;
+
+namespace Flake.Providers
+{
+    /// <summary>
+    /// A command provider that can only provide one type of
+    /// command.
+    /// </summary>
+    public sealed class SingleCommandProvider : ICommandProvider
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Flake.Providers.SingleCommandProvider"/> class.
+        /// </summary>
+        /// <param name="Command">The command to provide.</param>
+        public SingleCommandProvider(ICommand Command)
+        {
+            this.Command = Command;
+        }
+
+        /// <summary>
+        /// Gets the command that is provider wraps.
+        /// </summary>
+        /// <value>The command.</value>
+        public ICommand Command { get; private set; }
+
+        /// <inheritdoc/>
+        public ResultOrError<ICommand, LogEntry> GetCommand(
+            string Name, ICompilerLog Log)
+        {
+            if (Command.Name == Name)
+                return ResultOrError<ICommand, LogEntry>.CreateResult(Command);
+            else
+                return EmptyCommandProvider.Instance.GetCommand(Name, Log);
+        }
+    }
+}
+
---
+++ src/Flake/Providers/SingleTaskHandlerProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Flame.Compiler;
+
+namespace Flake.Providers
+{
+    /// <summary>
+    /// A command provider that can only provide one type of
+    /// command.
+    /// </summary>
+    public sealed class SingleTaskHandlerProvider : ITaskHandlerProvider
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Flake.Providers.SingleTaskHandlerProvider"/> class.
+        /// </summary>
+        /// <param name="Handler">The task handler.</param>
+        public SingleTaskHandlerProvider(ITaskHandler Handler)
+        {
+            this.Handler = Handler;
+        }
+
+        /// <summary>
+        /// Gets the command that is provider wraps.
+        /// </summary>
+        /// <value>The command.</value>
+        public ITaskHandler Handler { get; private set; }
+
+        /// <inheritdoc/>
+        public ResultOrError<ITaskHandler, LogEntry> GetHandler(
+            TaskDescription Description, ICompilerLog Log)
+        {
+            if (Handler.TaskType == Description.Type)
+                return ResultOrError<ITaskHandler, LogEntry>.CreateResult(Handler);
+            else
+                return EmptyTaskHandlerProvider.Instance.GetHandler(Description, Log);
+        }
+    }
+}
+
