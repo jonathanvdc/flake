f0948e99cd1c0aea6a3660b8fb9d5e4db0ba4042
jonathanvdc <jonathan.vdc@outlook.com>
2017-01-30 12:54:30 +0100

    Write usage message when no args are provided

--- src/Flake.Driver/Program.cs
+++ src/Flake.Driver/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using Flame.Front;
+using Flame.Front.Cli;
+using Flame.Compiler;
 
 namespace Flake.Driver
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main(string[] Args)
         {
-            Console.WriteLine("Hello World!");
+            // Acquire a console log.
+            var logOptions = EmptyCompilerOptions.Instance;
+            var log = new ConsoleLog(
+                ConsoleEnvironment.AcquireConsole(logOptions),
+                logOptions);
+
+            if (Args.Length == 0)
+            {
+                log.LogEvent(new LogEntry("usage", "flake command [args...]"));
+                return;
+            }
+
+            string commandName = Args[0];
         }
     }
 }
