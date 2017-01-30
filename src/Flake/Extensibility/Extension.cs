8e7692852b3df644161b6aeacadb6de1ca58fe14
jonathanvdc <jonathan.vdc@outlook.com>
2017-01-30 20:21:58 +0100

    Define class Extension, rename FlakeImport{=> Attribute}.cs

---
+++ src/Flake/Extensibility/Extension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flake.Extensibility
+{
+    /// <summary>
+    /// Describes what a flake extension entails.
+    /// </summary>
+    public sealed class Extension
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Flake.Extensibility.Extension"/> class.
+        /// </summary>
+        /// <param name="CommandProviders">The set of command providers.</param>
+        /// <param name="TaskHandlerProviders">Thet set of task handler providers.</param>
+        public Extension(
+            IEnumerable<ICommandProvider> CommandProviders,
+            IEnumerable<ITaskHandlerProvider> TaskHandlerProviders)
+        {
+            this.CommandProviders = CommandProviders;
+            this.TaskHandlerProviders = TaskHandlerProviders;
+        }
+
+        /// <summary>
+        /// Gets the extension's set of command providers.
+        /// </summary>
+        /// <value>The set of command providers.</value>
+        public IEnumerable<ICommandProvider> CommandProviders { get; private set; }
+
+        /// <summary>
+        /// Gets the extension's set of task handler providers.
+        /// </summary>
+        /// <value>The set of task handler providers.</value>
+        public IEnumerable<ITaskHandlerProvider> TaskHandlerProviders { get; private set; }
+    }
+}
+
